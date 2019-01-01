999e45db4c4e8593fcd16a1bb10c62a34d409d85
Ho Tien Vu <ho0001vu@gmail.com>
2019-01-02 00:17:08 +0800

    initial work on converting avro schema to beam schema

--- src/main/java/beamtest/examples/BeamSQLExample.java
+++ src/main/java/beamtest/examples/BeamSQLExample.java
@@ -1,6 +1,7 @@
 package beamtest.examples;
 
 
+import beamtest.examples.common.AvroUtils;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.extensions.sql.SqlTransform;
@@ -14,11 +15,11 @@ import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PCollectionTuple;
 import org.apache.beam.sdk.values.Row;
 import org.apache.beam.sdk.values.TupleTag;
-
 import javax.annotation.Nullable;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+
 
 /**
  * This is a quick example, which uses Beam SQL DSL to create a data pipeline.
@@ -34,90 +35,91 @@ import java.io.InputStream;
  * Beam documentation on how to run pipelines.
  */
 class BeamSqlExample {
-    private static org.apache.avro.Schema AVRO_SCHEMA;
-    private static Schema BEAM_SCHEMA;
-    static {
-        try (
+  private static org.apache.avro.Schema AVRO_SCHEMA;
+  private static Schema BEAM_SCHEMA;
+
+  static {
+    try (
             InputStream is = new FileInputStream("people.avsc")) {
-            AVRO_SCHEMA = new org.apache.avro.Schema.Parser().parse(is);
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-
-        BEAM_SCHEMA = Schema.builder()
-                .addStringField("name")
-                .addInt64Field("age")
-                .build();
+      AVRO_SCHEMA = new org.apache.avro.Schema.Parser().parse(is);
+    } catch (IOException e) {
+      e.printStackTrace();
+      System.exit(1);
     }
 
-    static class Address {
+    BEAM_SCHEMA = Schema.builder()
+            .addStringField("name")
+            .addInt64Field("age")
+            .build();
+  }
 
-    }
 
-    private static PCollection<Row> prepareData(Pipeline p, ParquetWordCount.WordCountOptions options) {
-        //define the input row format
-        return p.apply(FileIO.match().filepattern(options.getInputFile()))
-                .apply(FileIO.readMatches())
-                .apply(ParquetIO.readFiles(AVRO_SCHEMA))
-                .apply(MapElements.via(new SimpleFunction<GenericRecord, Row>() {
-                    @Override
-                    public Row apply(GenericRecord input) {
-                        String name = input.get("name").toString();
-                        long age = (long) input.get("age");
-                        return Row.withSchema(BEAM_SCHEMA).addValues(name, age).build();
-                    }
-                }))
-                .setRowSchema(BEAM_SCHEMA);
 
-    }
+  private static PCollection<Row> prepareData(Pipeline p, ParquetWordCount.WordCountOptions options) {
+    //define the input row format
+    Schema schema = AvroUtils.avroToBeamSchema(AVRO_SCHEMA);
+    return p.apply(FileIO.match().filepattern(options.getInputFile()))
+            .apply(FileIO.readMatches())
+            .apply(ParquetIO.readFiles(AVRO_SCHEMA))
+            .apply(MapElements.via(new SimpleFunction<GenericRecord, Row>() {
+              @Override
+              public Row apply(GenericRecord input) {
+                String name = input.get("name").toString();
+                long age = (long) input.get("age");
+                return Row.withSchema(schema).addValues(name, age, null).build();
+              }
+            }))
+            .setRowSchema(schema);
 
-    private static void queryData(PCollection<Row> inputTable) {
-        //Case 1. run a simple SQL query over input PCollection with BeamSql.simpleQuery;
-        PCollection<Row> outputStream =
-                inputTable.apply(SqlTransform.query("select name, age from PCOLLECTION where age >= 10"));
+  }
 
-        // print the output record of case 1;
-        outputStream.apply(
-                "log_result",
-                MapElements.via(
-                        new SimpleFunction<Row, Void>() {
-                            @Override
-                            public @Nullable
-                            Void apply(Row input) {
-                                System.out.println("PCOLLECTION: " + input.getValues());
-                                return null;
-                            }
-                        }));
+  private static void queryData(PCollection<Row> inputTable) {
+    //Case 1. run a simple SQL query over input PCollection with BeamSql.simpleQuery;
+    PCollection<Row> outputStream =
+            inputTable.apply(SqlTransform.query("select name, age from PCOLLECTION where age >= 10"));
 
-        // Case 2. run the query with SqlTransform.query over result PCollection of case 1.
-        PCollection<Row> outputStream2 =
-                PCollectionTuple.of(new TupleTag<>("CASE1_RESULT"), outputStream)
-                        .apply(SqlTransform.query("select name, sum(age) from CASE1_RESULT group by name"));
+    // print the output record of case 1;
+    outputStream.apply(
+            "log_result",
+            MapElements.via(
+                    new SimpleFunction<Row, Void>() {
+                      @Override
+                      public @Nullable
+                      Void apply(Row input) {
+                        System.out.println("PCOLLECTION: " + input.getValues());
+                        return null;
+                      }
+                    }));
 
-        // print the output record of case 2;
-        outputStream2.apply(
-                "log_result",
-                MapElements.via(
-                        new SimpleFunction<Row, Void>() {
-                            @Override
-                            public @Nullable Void apply(Row input) {
-                                // expect output:
-                                //  CASE1_RESULT: [row, 5.0]
-                                System.out.println("CASE1_RESULT: " + input.getValues());
-                                return null;
-                            }
-                        }));
-    }
+    // Case 2. run the query with SqlTransform.query over result PCollection of case 1.
+    PCollection<Row> outputStream2 =
+            PCollectionTuple.of(new TupleTag<>("CASE1_RESULT"), outputStream)
+                    .apply(SqlTransform.query("select name, sum(age) from CASE1_RESULT group by name"));
 
-    public static void main(String[] args) {
-        ParquetWordCount.WordCountOptions options = PipelineOptionsFactory.fromArgs(args)
-                .withValidation().as(ParquetWordCount.WordCountOptions.class);
-        Pipeline p = Pipeline.create(options);
-        PCollection<Row> inputTable = prepareData(p, options);
+    // print the output record of case 2;
+    outputStream2.apply(
+            "log_result",
+            MapElements.via(
+                    new SimpleFunction<Row, Void>() {
+                      @Override
+                      public @Nullable
+                      Void apply(Row input) {
+                        // expect output:
+                        //  CASE1_RESULT: [row, 5.0]
+                        System.out.println("CASE1_RESULT: " + input.getValues());
+                        return null;
+                      }
+                    }));
+  }
 
-        queryData(inputTable);
+  public static void main(String[] args) {
+    ParquetWordCount.WordCountOptions options = PipelineOptionsFactory.fromArgs(args)
+            .withValidation().as(ParquetWordCount.WordCountOptions.class);
+    Pipeline p = Pipeline.create(options);
+    PCollection<Row> inputTable = prepareData(p, options);
 
-        p.run().waitUntilFinish();
-    }
+    queryData(inputTable);
+
+    p.run().waitUntilFinish();
+  }
 }
